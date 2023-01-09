450040ec20ca2349bd182f1bd0da1c80c125cbf0
Kieran <kieran@andersons.bz>
2023-01-09 21:32:45 +0000

    Fixed bug

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,6 @@ use KieranFYI\UserUI\Http\Controllers\UserController;
 
 Admin::route()
     ->group(function () {
-        Route::get('users/{user}/ajax', [UserController::class])
-            ->name('users.ajax');
-
         Route::resource('users', UserController::class)
             ->except('edit');
     });
