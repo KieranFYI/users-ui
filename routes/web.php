6da29e0aa93fedc0a3e5b998d1e32f0007980c25
Kieran <kieran@andersons.bz>
2023-01-14 23:06:28 +0000

    Update dependencies

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use KieranFYI\Admin\Facades\Admin;
 use KieranFYI\UserUI\Http\Controllers\UserAPIController;
 use KieranFYI\UserUI\Http\Controllers\UserController;
 
-Admin::route()
-    ->group(function () {
+Admin::route(function () {
 
         Route::resource('users', UserController::class)
             ->only('index', 'create', 'show');
